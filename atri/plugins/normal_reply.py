978794ea0f90389e39b7c2e850130bb24eec915c
jiangyuxiaoxiao <caizichen@suzakuintsubaki.com>
2021-11-09 01:43:11 +0800

    ok

--- atri/plugins/normal_reply.py
+++ atri/plugins/normal_reply.py
@@ -1,7 +1,7 @@
 from nonebot import on_message
 from nonebot.rule import to_me
 from nonebot.typing import T_State
-from nonebot.adapters import Bot, Event
+from nonebot.adapters.cqhttp import Bot, Event
 import random
 # 引用字典
 import sys
@@ -13,21 +13,22 @@ reply=on_message(priority=5)
 
 @reply.handle()
 async def normal_reply(bot: Bot, event: Event, state: T_State):
-    msg = str(event.get_message()).strip()
+    msg = event.raw_message
     if msg in dictionary:
-        groupid = event.group_id
-        # 单条回复类
-        if not dictionary[msg]["multi_reply"]:
-            sndmsg = dictionary[msg]["reply"][0]
-
+        if event.message_type=='group':
+            groupid = event.group_id
+            # 单条回复类
+            if not dictionary[msg]["multi_reply"]:
+                sndmsg = dictionary[msg]["reply"][0]
+            else:
+                reply_len = len(dictionary[msg]["reply"])
+                # 选择随机词条
+                rand = random.randint(0, reply_len - 1)
+                sndmsg = dictionary[msg]["reply"][rand]
+            await bot.call_api("send_msg", **{"message_type": "group", "group_id": groupid,
+                                              "message": "{}".format(sndmsg)})
         else:
-            reply_len=len(dictionary[msg]["reply"])
-            # 选择随机词条
-            rand=random.randint(0,reply_len-1)
-            sndmsg = dictionary[msg]["reply"][rand]
-
-        await bot.call_api("send_msg", **{"message_type": "group", "group_id": groupid,
-                                          "message": "{}".format(sndmsg)})
+            return
 
 
 
