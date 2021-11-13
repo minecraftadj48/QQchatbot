a31db031dfddb16805e1d68772b304334219c5cf
jiangyuxiaoxiao <caizichen@suzakuintsubaki.com>
2021-11-14 00:28:17 +0800

    ok

--- atri/plugins/users_management.py
+++ atri/plugins/users_management.py
@@ -14,9 +14,11 @@ scheduler = require("nonebot_plugin_apscheduler").scheduler
 # 群事件处理
 group_handler = on_notice(priority=15)
 # 指令处理
-command_handler = on_command("用户刷新",priority=10)
+command_handler = on_command("用户刷新", priority=10)
+
 
 # 定期更新数据库
+# 为定期执行的任务，执行时间为传入的hour minute参数
 @scheduler.scheduled_job("cron", hour="00", minute="00")
 async def update_usersdata():
     # 传入bot对象
@@ -25,25 +27,54 @@ async def update_usersdata():
     connect = sqlite3.connect(".\\Bot_data\\SQLite\\Users.db")
     # 创建游标
     cursor = connect.cursor()
+
+    # 群组用户更新
     # 遍历群组
     groups = await bot.call_api("get_group_list")
+    # groups为群组列表
     for group in groups:
+        # group_id 群号
         group_id = group["group_id"]
+        # group_member 群成员列表
         group_members = await bot.call_api("get_group_member_list", **{"group_id": group_id})
         for group_member in group_members:
+            # member_id 群成员QQ号
             member_id = group_member["user_id"]
             # 将member_id放入元组中进行数据传入
-            member_id_tuple=(member_id,)
+            member_id_tuple = (member_id,)
+            # member_name 群成员QQ名
             member_name = group_member["nickname"]
-            cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);",member_id_tuple)
+            # 查找数据库中是否存在该成员
+            cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", member_id_tuple)
             for c in cursor:
                 if c[0] == 0:
+                    # 避免SQL注入攻击，使用？来代替字符串替换
                     insert_value = (member_id, member_name)
                     connect.execute('''
                                     INSERT INTO 
                                     BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
                                     VALUES(2,0,0,?,?);
                                     ''', insert_value)
+
+    # 好友用户更新 api返回json数组 详见go—cqhttp的文档
+    friends = await bot.call_api("get_friend_list")
+    for friend in friends:
+        # 好友QQ
+        friend_id = friend["user_id"]
+        # 好友名
+        friend_name = friend["nickname"]
+        # 查找数据库中是否存在该成员
+        cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", member_id_tuple)
+        for c in cursor:
+            if c[0] == 0:
+                # 避免SQL注入攻击，使用？来代替字符串替换
+                insert_value = (friend_id, friend_name)
+                connect.execute('''
+                                INSERT INTO 
+                                BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
+                                VALUES(2,0,0,?,?);
+                                ''', insert_value)
+    # 数据库保存更新
     cursor.close()
     connect.commit()
     connect.close()
@@ -59,26 +90,27 @@ async def add_member_to_user_database(bot: Bot, event: Event, state: T_State):
         member_id = event.user_id
         # 将member_id放入元组中进行数据传入
         member_id_tuple = (member_id,)
-        info = await bot.call_api("get_group_member_info", **{"group_id": group_id,"user_id":member_id})
+        info = await bot.call_api("get_group_member_info", **{"group_id": group_id, "user_id": member_id})
         member_name = info["nickname"]
         # 连接数据库
         connect = sqlite3.connect(".\\Bot_data\\SQLite\\Users.db")
         # 创建游标
         cursor = connect.cursor()
         # 判断是否已在数据库内
-        cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);",member_id_tuple)
+        cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", member_id_tuple)
         for c in cursor:
             if c[0] == 0:
                 # 将用户添加入数据库
                 insert_value = (member_id, member_name)
                 connect.execute('''
-                                                    INSERT INTO 
-                                                    BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
-                                                    VALUES(2,0,0,?,?);
-                                                    ''', insert_value)
+                                INSERT INTO 
+                                BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
+                                VALUES(2,0,0,?,?);
+                                ''', insert_value)
         cursor.close()
         connect.commit()
         connect.close()
+
 
 # 接收指令进行刷新
 @command_handler.handle()
@@ -87,40 +119,67 @@ async def refresh_user_database(bot: Bot, event: Event, state: T_State):
     connect = sqlite3.connect(".\\Bot_data\\SQLite\\Users.db")
     # 创建游标
     cursor = connect.cursor()
-    #权限审查
+    # 权限审查
     caller = event.user_id
     caller = (caller,)
-    cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);",caller)
-    #检查是否在数据库中
+    cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", caller)
+    # 检查是否在数据库中
     for c in cursor:
         if c[0] == 0:
             return
         else:
-            #检查是否满足权限
-            cursor.execute("SELECT PERMISSION FROM BASE WHERE QID=(?)",caller)
+            # 检查是否满足权限
+            cursor.execute("SELECT PERMISSION FROM BASE WHERE QID=(?)", caller)
             for c in cursor:
                 if not c[0] == 0:
                     return
+    # 群组用户更新
     # 遍历群组
     groups = await bot.call_api("get_group_list")
+    # groups为群组列表
     for group in groups:
+        # group_id 群号
         group_id = group["group_id"]
+        # group_member 群成员列表
         group_members = await bot.call_api("get_group_member_list", **{"group_id": group_id})
         for group_member in group_members:
+            # member_id 群成员QQ号
             member_id = group_member["user_id"]
             # 将member_id放入元组中进行数据传入
             member_id_tuple = (member_id,)
+            # member_name 群成员QQ名
             member_name = group_member["nickname"]
-            cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);",member_id_tuple)
+            # 查找数据库中是否存在该成员
+            cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", member_id_tuple)
             for c in cursor:
                 if c[0] == 0:
+                    # 避免SQL注入攻击，使用？来代替字符串替换
                     insert_value = (member_id, member_name)
                     connect.execute('''
-                                    INSERT INTO 
-                                    BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
-                                    VALUES(2,0,0,?,?);
-                                    ''', insert_value)
-    #
+                                       INSERT INTO 
+                                       BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
+                                       VALUES(2,0,0,?,?);
+                                       ''', insert_value)
+
+    # 好友用户更新 api返回json数组 详见go—cqhttp的文档
+    friends = await bot.call_api("get_friend_list")
+    for friend in friends:
+        # 好友QQ
+        friend_id = friend["user_id"]
+        # 好友名
+        friend_name = friend["nickname"]
+        # 查找数据库中是否存在该成员
+        cursor.execute("SELECT COUNT(*) FROM BASE WHERE QID=(?);", member_id_tuple)
+        for c in cursor:
+            if c[0] == 0:
+                # 避免SQL注入攻击，使用？来代替字符串替换
+                insert_value = (friend_id, friend_name)
+                connect.execute('''
+                                   INSERT INTO 
+                                   BASE(PERMISSION,ATTITUDE,GOLD,QID,QQNAME) 
+                                   VALUES(2,0,0,?,?);
+                                   ''', insert_value)
+    # 数据库保存更新
     cursor.close()
     connect.commit()
     connect.close()
